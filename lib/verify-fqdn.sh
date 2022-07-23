6e1088fa5a87ae6c2a5a00b9607810f0bfe08611
NineWhiteDream <109854552+NineWhiteDream@users.noreply.github.com>
2022-07-23 15:08:09 +0300

    Add files via upload

---
+++ lib/verify-fqdn.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+set -e
+
+#############################################################################
+#                                                                           #
+# Project 'pterodactyl-installer'                                           #
+#                                                                           #
+# Copyright (C) 2018 - 2022, Vilhelm Prytz, <vilhelm@prytznet.se>           #
+#                                                                           #
+#   This program is free software: you can redistribute it and/or modify    #
+#   it under the terms of the GNU General Public License as published by    #
+#   the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                     #
+#                                                                           #
+#   This program is distributed in the hope that it will be useful,         #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#   GNU General Public License for more details.                            #
+#                                                                           #
+#   You should have received a copy of the GNU General Public License       #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
+#                                                                           #
+# https://github.com/vilhelmprytz/pterodactyl-installer/blob/master/LICENSE #
+#                                                                           #
+# This script is not associated with the official Pterodactyl Project.      #
+# https://github.com/vilhelmprytz/pterodactyl-installer                     #
+#                                                                           #
+#############################################################################
+
+CHECKIP_URL="https://checkip.pterodactyl-installer.se"
+DNS_SERVER="8.8.8.8"
+
+# exit with error status code if user is not root
+if [[ $EUID -ne 0 ]]; then
+  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  exit 1
+fi
+
+# check for curl
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+output() {
+  echo "* $1"
+}
+
+error() {
+  COLOR_RED='\033[0;31m'
+  COLOR_NC='\033[0m'
+
+  echo ""
+  echo -e "* ${COLOR_RED}ERROR${COLOR_NC}: $1"
+  echo ""
+}
+
+fail() {
+  output "The DNS record ($dns_record) does not match your server IP. Please make sure the FQDN $fqdn is pointing to the IP of your server, $ip"
+  output "If you are using Cloudflare, please disable the proxy or opt out from Let's Encrypt."
+
+  echo -n "* Proceed anyways (your install will be broken if you do not know what you are doing)? (y/N): "
+  read -r override
+
+  [[ ! "$override" =~ [Yy] ]] && error "Invalid FQDN or DNS record" && exit 1
+  return 0
+}
+
+dep_install() {
+  [ "$os" == "centos" ] && yum install -q -y bind-utils
+  [ "$os" == "debian" ] && apt-get install -y dnsutils -qq
+  [ "$os" == "ubuntu" ] && apt-get install -y dnsutils -qq
+  return 0
+}
+
+confirm() {
+  output "This script will perform a HTTPS request to the endpoint $CHECKIP_URL"
+  output "The official check-IP service for this script, https://checkip.pterodactyl-installer.se"
+  output "- will not log or share any IP-information with any third-party."
+  output "If you would like to use another service, feel free to modify the script."
+
+  echo -e -n "* I agree that this HTTPS request is performed (y/N): "
+  read -r confirm
+  [[ "$confirm" =~ [Yy] ]] || (error "User did not agree" && false)
+}
+
+dns_verify() {
+  output "Resolving DNS for $fqdn"
+  ip=$(curl -4 -s $CHECKIP_URL)
+  dns_record=$(dig +short @$DNS_SERVER "$fqdn" | tail -n1)
+  [ "${ip}" != "${dns_record}" ] && fail
+  output "DNS verified!"
+}
+
+main() {
+  fqdn="$1"
+  os="$2"
+  dep_install
+  confirm && dns_verify
+  true
+}
+
+main "$1" "$2"
